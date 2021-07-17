f614f9bf47c226558cd160e0516b398e7043111b
ansagan <islamgaliev@mail.ru>
2021-07-17 14:49:59 +0500

    folder namings were changed to more meaningfull ones

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 import './api.js';
-import './upload/pictures-upload.js';
-import './pictures/pictures.js';
-import './pictures/filter.js';
+import './editing/pictures-upload.js';
+import './browsing/pictures.js';
+import './browsing/filter.js';
--- js/upload/pictures-upload.js
+++
@@ -1,8 +0,0 @@
-const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-const uploadFile = document.querySelector('#upload-file');
-const onUploadFileClick = (evt) => {
-  const file = evt.target.files[0];
-  console.log(file);
-};
-
-uploadFile.addEventListener('change', onUploadFileClick);
