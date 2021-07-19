c3d20ac404fd4eab33964150314c0270a7e46119
ansagan <islamgaliev@mail.ru>
2021-07-19 15:28:35 +0500

    new Form for validation of the hashtag and description

---
+++ js/editing/validation.js
@@ -0,0 +1,100 @@
+import { onEscapeKeydown } from './picture-edit.js';
+import { hasDuplicateElements } from '../utils.js';
+import { sendForm } from '../api.js';
+
+const HASHTAG_MAX_LENGTH = 20;
+const HASHTAG_MAX_COUNT = 5;
+const HASHTAG_PATTERN = /^#[a-z0-9]{1,19}$/;
+
+const form = document.querySelector('.img-upload__form');
+const hashtags = form.querySelector('.text__hashtags');
+const description = form.querySelector('.text__description');
+
+const onHashtagsOrDescriptionFocus = () => {
+  document.removeEventListener('keydown', onEscapeKeydown);
+};
+
+const onHashtagsOrDescriptionBlur = () => {
+  document.addEventListener('keydown', onEscapeKeydown);
+};
+
+const isHashtagsValid = (input) => {
+  if (input.length > HASHTAG_MAX_COUNT) {
+    return 'нельзя указать больше пяти хэш-тегов';
+  }
+
+  if (hasDuplicateElements(input)) {
+    return 'один и тот же хэш-тег не может быть использован дважды';
+  }
+
+  for (const hashtag of input) {
+    if (!hashtag.startsWith('#')) {
+      return 'хэш-тег начинается с символа # (решётка)';
+    }
+
+    if (hashtag === '#') {
+      return 'хеш-тег не может состоять только из одной решётки';
+    }
+
+    if (hashtag.length > HASHTAG_MAX_LENGTH) {
+      return 'максимальная длина одного хэш-тега 20 символов, включая решётку';
+    }
+
+    if (!HASHTAG_PATTERN.test(hashtag)) {
+      return 'хеш-тег может состоять только из букв и чисел';
+    }
+  }
+
+  return 'valid';
+};
+
+const isDescriptionValid = (input) => {
+  if (input.length > 140) {
+    return 'длина комментария не может составлять больше 140 символов';
+  }
+  return 'valid';
+};
+
+const validateInput = (element, message) => {
+  if (message !== 'valid') {
+    element.setCustomValidity(message);
+    element.reportValidity();
+    return false;
+  } else {
+    element.setCustomValidity('');
+    element.reportValidity();
+    return true;
+  }
+};
+
+const validateHashtags = () => {
+  if (hashtags.value.length === 0) {
+    return validateInput(hashtags, 'valid');
+  }
+
+  const hashtagsSpilted = hashtags.value
+    .toLowerCase()
+    .replace(/\s+/g, ' ')
+    .trim()
+    .split(' ');
+  return validateInput(hashtags, isHashtagsValid(hashtagsSpilted));
+};
+
+const validateDescription = () =>
+  validateInput(description, isDescriptionValid(description.value));
+
+const onFormSubmit = (evt) => {
+  const validHashtags = validateHashtags();
+  const validDescription = validateDescription();
+
+  if (validHashtags && validDescription) {
+    sendForm(new FormData());
+  }
+  evt.preventDefault();
+};
+
+hashtags.addEventListener('focus', onHashtagsOrDescriptionFocus);
+hashtags.addEventListener('blur', onHashtagsOrDescriptionBlur);
+description.addEventListener('focus', onHashtagsOrDescriptionFocus);
+description.addEventListener('blur', onHashtagsOrDescriptionBlur);
+form.addEventListener('submit', onFormSubmit);
