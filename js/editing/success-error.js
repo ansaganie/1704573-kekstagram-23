c8289741b1ad2e2a47b7b703c16714b4b6ec654e
ansagan <islamgaliev@mail.ru>
2021-07-21 10:13:50 +0500

    functions for showing success, error, and loading messages were created

--- js/editing/success-error.js
+++ js/editing/success-error.js
@@ -1,33 +1,82 @@
-const removeSuccessMessage = () => {
-  const successTemplate = document
-    .querySelector('#success').content
-    .querySelector('.success')
-    .cloneNode(true);
+import { isEscapePressed } from '../utils.js';
+
+const EVENT_HANDLER_OPTIONS = { once: true };
+
+const destroyElement = (element) => {
+  if (element.parentNode) {
+    element.parentNode.removeChild(element);
+  }
 };
 
-const onEscapeKeydown = () => {
-  
-}
-
 const showSuccessMessage = () => {
-  const successTemplate = document
-    .querySelector('#success').content
-    .querySelector('.success')
+  const successSection = document
+    .querySelector('#success')
+    .content.querySelector('.success')
     .cloneNode(true);
+
+  document.body.appendChild(successSection);
+
+  const onEscapeKeydown = (evt) => {
+    if (isEscapePressed(evt)) {
+      destroyElement(successSection);
+    }
+  };
+
+  const onSuccessSectionClick = () => {
+    destroyElement(successSection);
+    document.removeEventListener('keydown', onEscapeKeydown);
+  };
+
+  document.addEventListener('keydown', onEscapeKeydown);
+  successSection.addEventListener('click', onSuccessSectionClick);
 };
 
 const showErrorMessage = () => {
-  const successTemplate = document
-    .querySelector('#error').content
-    .querySelector('.error')
+  const errorSection = document
+    .querySelector('#error')
+    .content.querySelector('.error')
     .cloneNode(true);
+
+  document.body.appendChild(errorSection);
+
+  const onEscapeKeydown = (evt) => {
+    if (isEscapePressed(evt)) {
+      destroyElement(errorSection);
+    }
+  };
+
+  const onErrorSectionClick = () => {
+    destroyElement(errorSection);
+    document.removeEventListener('keydown', onEscapeKeydown);
+  };
+
+  document.addEventListener('keydown', onEscapeKeydown);
+  errorSection.addEventListener(
+    'click',
+    onErrorSectionClick,
+    EVENT_HANDLER_OPTIONS,
+  );
 };
 
-const removeErrorMessage = () => {
-  const successTemplate = document
-    .querySelector('#error').content
-    .querySelector('.error')
+const showLoadingMessage = () => {
+  const messageLoading = document
+    .querySelector('#messages').content
+    .querySelector('.img-upload__message--loading')
     .cloneNode(true);
+  document.body.appendChild(messageLoading);
 };
 
-export { showSuccessMessage, showErrorMessage };
+const hideLoadingMessage = () => {
+  const messageLoading = document
+    .querySelector('.img-upload__message--loading');
+  if (messageLoading) {
+    messageLoading.parentElement.removeChild(messageLoading);
+  }
+};
+
+export {
+  showSuccessMessage,
+  showErrorMessage,
+  showLoadingMessage,
+  hideLoadingMessage
+};
