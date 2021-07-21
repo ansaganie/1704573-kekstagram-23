2aee77679055af950f70a4589dcf3bba0593ba15
ansagan <islamgaliev@mail.ru>
2021-07-22 00:03:54 +0500

    added stopPropagation function; Unused functions were deleted

--- js/utils.js
+++ js/utils.js
@@ -48,33 +48,9 @@ const getRandomPositiveInteger = (first, second) => {
   return Math.floor(result);
 };
 
-// Функция взята из интернета и доработана
-// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_throttle
-
-const throttle = (callback, delayBetweenFrames) => {
-  // Используем замыкания, чтобы время "последнего кадра" навсегда приклеилось
-  // к возвращаемой функции с условием, тогда мы его сможем перезаписывать
-  let lastTime = 0;
-
-  return (...rest) => {
-    // Получаем текущую дату в миллисекундах,
-    // чтобы можно было в дальнейшем
-    // вычислять разницу между кадрами
-    const now = new Date();
-
-    // Если время между кадрами больше задержки,
-    // вызываем наш колбэк и перезаписываем lastTime
-    // временем "последнего кадра"
-    if (now - lastTime >= delayBetweenFrames) {
-      callback.apply(this, rest);
-      lastTime = now;
-    }
-  };
-};
-
-const checkStringLength = (string, length) => string.length <= length;
-
 const isEscapePressed = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
+
+const isEnterPressed = (evt) => evt.key === 'Enter';
 
 const hasDuplicateElements = (array) => new Set(array).size !== array.length;
 
@@ -84,12 +60,14 @@ const destroyElement = (element) => {
   }
 };
 
+const stopPropagation = (evt) => evt.stopPropagation();
+
 export {
   debounce,
-  throttle,
-  checkStringLength,
   getRandomPositiveInteger,
   isEscapePressed,
+  isEnterPressed,
   hasDuplicateElements,
-  destroyElement
+  destroyElement,
+  stopPropagation
 };
